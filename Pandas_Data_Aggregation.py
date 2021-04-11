3db1fd75fd43a7928dfdd3188ab87e298ec8583c
Olga Lazarenko <olazarenko@outlook.com>
2021-04-11 13:24:40 -0400

    read <brige> dataset into a new dataframe <brige_df>

--- Pandas_Data_Aggregation.py
+++ Pandas_Data_Aggregation.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 print()
 
-auto = pd.read_csv('E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\Autos_Import_1985.csv' ,
+auto_df = pd.read_csv('E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\Autos_Import_1985.csv' ,
                         usecols = [ 'Symboling' ,
                                     'Make' ,
                                     'Body Style' , 
@@ -15,6 +15,23 @@ auto = pd.read_csv('E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\A
                                     'Price']
                         )
 # print the first 5 rows of the file
-print(auto.head())
+print('Auto dataframe:')
+print(auto_df.head())
 print()
-print(auto.tail(10))
+print(auto_df.tail(10))
+print()
+
+# read the file <Briges_1818_1986.csv> info <brige> dataframe
+brige_df = pd.read_csv('E:\_Python_Projects_Data\Data_Visualization\Briges_Data_Set\Briges_1818_1986.csv' , 
+                        usecols = ['River' ,
+                                    'Erected' ,
+                                    'Purpose' ,
+                                    'Length' ,
+                                    'Material' ,
+                                    'Type'])
+
+# print the first 5 rows of the file
+print('Briges dataframe:')
+print(brige_df.head())
+print()
+print(brige_df.tail(10))
